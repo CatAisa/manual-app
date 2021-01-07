40973142a06f1018efcc3e72df1ff94e5b46ad48
CatAisa <suzuki03331@icloud.com>
2021-01-07 20:51:56 +0900

    manualモデルのバリデーションとアソシエーションを設定

--- app/models/manual.rb
+++ app/models/manual.rb
@@ -1,2 +1,11 @@
 class Manual < ApplicationRecord
+  belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category_id
+
+  with_options presence: true do
+    validates :title
+    validates :category_id, numericality: {other_than: 0, message: "is invalid. Select status"}
+  end
 end
