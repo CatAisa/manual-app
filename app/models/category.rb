ab97d3bb37c64fbd8cf2cf28bee86974ed30c341
CatAisa <suzuki03331@icloud.com>
2021-01-07 20:51:18 +0900

    categoryモデルのテーブルとアソシエーション を仮設定

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,11 @@
-class Category < ApplicationRecord
+class Category < ActiveHash::Base
+  self.data = [
+    {id: 0, name: '--'}, 
+    {id: 1, name: '料理レシピ'}, 
+    {id: 2, name: '作業手順'}, 
+    {id: 3, name: '操作方法'}
+  ]
+
+  include ActiveHash::Associations
+  has_many :manuals
 end
