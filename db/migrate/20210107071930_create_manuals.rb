5c359c375f5d3f8a5913a9148e610a032f154b03
CatAisa <suzuki03331@icloud.com>
2021-01-07 16:25:55 +0900

    manualモデルとテーブルを作成

---
+++ db/migrate/20210107071930_create_manuals.rb
@@ -0,0 +1,11 @@
+class CreateManuals < ActiveRecord::Migration[6.0]
+  def change
+    create_table :manuals do |t|
+      t.string :title, null: false
+      t.text :description
+      t.integer :category_id, null: false
+      t.references :user, null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
