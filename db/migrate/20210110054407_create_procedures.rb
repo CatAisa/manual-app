a8c272be3a16bbd07431b1807c3c7e7c70248b85
CatAisa <suzuki03331@icloud.com>
2021-01-10 14:51:41 +0900

    proceduresテーブルの作成

--- db/migrate/20210110054407_create_procedures.rb
+++ db/migrate/20210110054407_create_procedures.rb
@@ -1,7 +1,9 @@
 class CreateProcedures < ActiveRecord::Migration[6.0]
   def change
     create_table :procedures do |t|
-
+      t.string :title, null: false
+      t.text :description
+      t.references :manual, null: false, foreign_key: true
       t.timestamps
     end
   end
