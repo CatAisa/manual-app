1ae7577af1fe43ddc024d710ce0fc18741eb921c
CatAisa <suzuki03331@icloud.com>
2021-01-10 15:02:41 +0900

    マニュアル手順作成画面の表示とリンクの追加

--- app/controllers/procedures_controller.rb
+++ app/controllers/procedures_controller.rb
@@ -1,2 +1,8 @@
 class ProceduresController < ApplicationController
+  def new
+    @manual = Manual.find(params[:manual_id])
+  end
+
+  def create
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "manuals#index"
-  resources :manuals, only: [:new, :create, :show]
+  resources :manuals, only: [:new, :create, :show] do
+    resources :procedures, only: [:new, :create]
+  end
   resources :users, only: :show
 end
