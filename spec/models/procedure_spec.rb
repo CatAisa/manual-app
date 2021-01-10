254143b841323d301e1908592adaaca05b179831
CatAisa <suzuki03331@icloud.com>
2021-01-10 22:00:23 +0900

    procedureモデルのテストコードを実施

--- spec/models/procedure_spec.rb
+++ spec/models/procedure_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe Procedure, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @procedure = FactoryBot.build(:procedure)
+  end
+
+  describe 'マニュアル手順新規作成' do
+    context 'マニュアル手順情報を保存できるとき' do
+      it "title, image, descriptionが存在すれば保存できる" do
+        expect(@procedure).to be_valid
+      end
+      it "imageが空でも保存できる" do
+        @procedure.image = nil
+        expect(@procedure).to be_valid
+      end
+      it "descriptionが空でも保存できる" do
+        @procedure.description = nil
+        expect(@procedure).to be_valid
+      end
+    end
+
+    context 'マニュアル手順情報を保存できないとき' do
+      it "titleが空だと保存できない" do
+        @procedure.title = nil
+        @procedure.valid?
+        expect(@procedure.errors.full_messages).to include("Title can't be blank")
+      end
+      it "manualが紐付いていないと保存できない" do
+        @procedure.manual = nil
+        @procedure.valid?
+        expect(@procedure.errors.full_messages).to include("Manual must exist")
+      end
+      it "userが紐付いていないと保存できない" do
+        @procedure.user = nil
+        @procedure.valid?
+        expect(@procedure.errors.full_messages).to include("User must exist")
+      end
+    end
+  end
 end
