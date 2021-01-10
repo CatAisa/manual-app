9e4d3b312011b7f281e86592df5c1073108c78ed
CatAisa <suzuki03331@icloud.com>
2021-01-10 20:04:53 +0900

    マニュアル手順の保存機能を実装

--- app/controllers/procedures_controller.rb
+++ app/controllers/procedures_controller.rb
@@ -1,8 +1,22 @@
 class ProceduresController < ApplicationController
   def new
     @manual = Manual.find(params[:manual_id])
+    @procedure = Procedure.new
   end
 
   def create
+    @manual = Manual.find(params[:manual_id])
+    @procedure = @manual.procedures.new(procedure_params)
+    if @procedure.save
+      redirect_to manual_path(@manual)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def procedure_params
+    params.require(:procedure).permit(:title, :image, :description).merge(user_id: current_user.id)
   end
 end
