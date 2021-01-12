38140e8751e45a50f494415cc89ffb076f16f8f8
CatAisa <suzuki03331@icloud.com>
2021-01-12 11:25:52 +0900

    手順を作成したユーザーしか編集をできない機能を実装

--- app/controllers/procedures_controller.rb
+++ app/controllers/procedures_controller.rb
@@ -1,8 +1,8 @@
 class ProceduresController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :manual_find, only: [:new, :create]
-  before_action :user_judge, only: [:new, :create]
+  before_action :manual_find, only: [:new, :create, :edit, :update]
   before_action :procedure_find, only: [:edit, :update]
+  before_action :user_judge, only: [:new, :create, :edit, :update]
 
   def new
     @procedure = Procedure.new
@@ -38,12 +38,11 @@ class ProceduresController < ApplicationController
     @manual = Manual.find(params[:manual_id])
   end
 
-  def user_judge
-    redirect_to root_path if current_user.id != @manual.user.id
+  def procedure_find
+    @procedure = @manual.procedures.find(params[:id])
   end
 
-  def procedure_find
-    @manual = Manual.find(params[:manual_id])
-    @procedure = @manual.procedures.find(params[:id])
+  def user_judge
+    redirect_to root_path if current_user.id != @manual.user.id
   end
 end
