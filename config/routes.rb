b8bc2879ee0b36cda6bada1f28589c29f67e945a
CatAisa <suzuki03331@icloud.com>
2021-01-19 20:50:10 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "manuals#index"
   resources :manuals, except: :index do
-    resources :procedures, except: [:index, :show]
+    resources :procedures, except: [:index, :show] do
+      resources :comments, only: :create
+    end
   end
   resources :users, only: :show
 end
