d8428f74272df509d0a92d256b139961ed4043fa
CatAisa <suzuki03331@icloud.com>
2021-01-24 19:37:34 +0900

    公開中のマニュアルも削除できるように修正

--- app/models/manual.rb
+++ app/models/manual.rb
@@ -3,7 +3,7 @@ class Manual < ApplicationRecord
   has_one_attached :image
   has_many :procedures, dependent: :destroy
   has_many :comments, dependent: :destroy
-  has_one :release
+  has_one :release, dependent: :destroy
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
