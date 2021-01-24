2a629576f7f4a740caa71d5c0ee577dd41f7f14c
CatAisa <suzuki03331@icloud.com>
2021-01-24 13:52:12 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     resources :procedures, except: [:index, :show] do
       resources :comments, only: [:create, :destroy]
     end
+    resources :releases, only: [:create, :destroy]
   end
   resources :users, only: :show
 end
