0a2694ac6f799744dc75829a910bc0513ae9850b
CatAisa <suzuki03331@icloud.com>
2021-01-25 18:58:02 +0900

    Releaseモデルのテストコードを実施

--- spec/models/release_spec.rb
+++ spec/models/release_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Release, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @release = FactoryBot.build(:release)
+  end
+
+  describe '一般公開の設定' do
+    context '一般公開が設定できるとき' do
+      it 'user_id, manual_idが存在すれば設定できる' do
+        expect(@release).to be_valid
+      end
+    end
+
+    context '一般公開が設定できないとき' do
+      it 'userが紐付いていないと設定できない' do
+        @release.user = nil
+        @release.valid?
+        expect(@release.errors.full_messages).to include('User translation missing: ja.activerecord.errors.models.release.attributes.user.required')
+      end
+      it 'manualが紐付いていないと設定できない' do
+        @release.manual = nil
+        @release.valid?
+        expect(@release.errors.full_messages).to include('Manual translation missing: ja.activerecord.errors.models.release.attributes.manual.required')
+      end
+    end
+  end
 end
