92909497643dddf9a4eb1e722bc8015bcd3a8502
CatAisa <suzuki03331@icloud.com>
2021-01-27 14:17:08 +0900

    「いいね」投稿機能の実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,12 @@
 class LikesController < ApplicationController
+  def create
+    Like.create(like_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def like_params
+    params.permit.merge(user_id: current_user.id, manual_id: params[:manual_id])
+  end
 end
