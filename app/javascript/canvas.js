b1e8c15ed559cf86008935b09a0e2d6fd65b0164
CatAisa <suzuki03331@icloud.com>
2021-02-02 14:55:16 +0900

    添付ファイルがプレビュー表示される

--- app/javascript/canvas.js
+++ app/javascript/canvas.js
@@ -1,14 +1,23 @@
 function canvasTitle() {
   console.log("load");
 
-  const canvas = document.getElementById("canvas");
-  const ctx = canvas.getContext("2d");
-  const image = new Image();
-  
-  image.src = "/images//test_image.jpg";
-  image.onload = () => {
-    ctx.drawImage(image, 0, 0, 500, 500/image.width*image.height);
-  }
+  fileField = document.getElementById("file-field");
+  fileField.addEventListener("change", (e) => {
+    console.log(e);
+
+    const file = e.target.files[0];
+    const blob = window.URL.createObjectURL(file);
+
+    const canvas = document.getElementById("canvas");
+    const ctx = canvas.getContext("2d");
+    const image = new Image();
+
+    image.src = blob;
+    image.onload = () => {
+      const imageHeight = 500 / image.width * image.height;
+      ctx.drawImage(image, 0, 0, 500, imageHeight);
+    };
+  });
 };
 
 if ( document.URL.match(/new/) ) {
