e00813113e86406331ffb4bc629ebe72ff40c7fc
CatAisa <suzuki03331@icloud.com>
2021-02-06 16:01:10 +0900

    一部メソッドをモデルに移動

--- app/controllers/procedures_controller.rb
+++ app/controllers/procedures_controller.rb
@@ -21,10 +21,10 @@ class ProceduresController < ApplicationController
 
       if Rails.env == "development"
         # Local environment
-        image_attach_local(procedure, decoded_url, filename)
+        procedure.image_attach_local(procedure, decoded_url, filename)
       elsif Rails.env == "production"
         # Production environment
-        image_attach_production(procedure, decoded_url, filename)
+        procedure.image_attach_production(procedure, decoded_url, filename)
       end
     end
 
@@ -71,21 +71,5 @@ class ProceduresController < ApplicationController
 
   def user_judge
     redirect_to root_path if current_user.id != @manual.user.id
-  end
-
-  def image_attach_local(procedure, decoded_url, filename)
-    File.open("#{Rails.root}/tmp/images/#{filename}", "wb") do |f|
-      f.write(decoded_url)
-    end
-    procedure.image.attach(io: File.open("#{Rails.root}/tmp/images/#{filename}"), filename: filename)
-    FileUtils.rm("#{Rails.root}/tmp/images/#{filename}")
-  end
-
-  def image_attach_production(procedure, decoded_url, filename)
-    File.open("/tmp/#{filename}", "wb") do |f|
-      f.write(decoded_url)
-    end
-    procedure.image.attach(io: File.open("/tmp/#{filename}"), filename: filename)
-    FileUtils.rm("/tmp/#{filename}")
   end
 end
