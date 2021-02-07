2af5c970a0d5b94f1a1d3765ce284ab53d61eaa4
CatAisa <suzuki03331@icloud.com>
2021-02-07 18:56:49 +0900

    js：変数定義

--- app/javascript/canvas.js
+++ app/javascript/canvas.js
@@ -1,10 +1,35 @@
 function canvasTitle() {
+  let clickFig = 0;
+  let crw = 0;
+  let crv = 0;
+  let rw = 300;
+  let rv = 300;
+
+  // Input image
+  const cnvCrop = document.getElementById("crop-canvas");
+  const ctxCrop = cnvCrop.getContext("2d");
+  const imgCrop = new Image();
+
+  // Red border
+  const cnvRed = document.getElementById("crop-area");
+  const ctxRed = cnvRed.getContext("2d");
+  const imgRed = new Image();
+
+  // Output image
   const canvas = document.getElementById("canvas");
   const ctx = canvas.getContext("2d");
   const image = new Image();
 
+  // Canvas size
+  const cw = cnvCrop.width;
+  const cv = cnvCrop.height;
+  const ow = canvas.width;
+  const ov = canvas.height;
+
   const fileField = document.getElementById("file-field");
   fileField.addEventListener("change", (e) => {
+    ctxCrop.clearRect(0, 0, cw, cv);
+    ctx.clearRect(0, 0, ow, ov);
     const file = e.target.files[0];
     const blob = window.URL.createObjectURL(file);
 
