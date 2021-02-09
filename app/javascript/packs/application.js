b9024580f26efdf9f1167b5d87a7af41cdbbecac
CatAisa <suzuki03331@icloud.com>
2021-02-09 22:40:20 +0900

    マニュアルに対するコメント投稿機能を実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,7 @@ require("../comment_delete")
 require("../canvas")
 require("../form")
 require("../drawing")
+require("../review_post")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
