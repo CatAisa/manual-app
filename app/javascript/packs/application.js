a75489235df4db3f1ac63706b1d1ebf17cd2488a
CatAisa <suzuki03331@icloud.com>
2021-02-09 22:51:12 +0900

    マニュアルに対するコメントの削除機能を実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,6 +12,7 @@ require("../canvas")
 require("../form")
 require("../drawing")
 require("../review_post")
+require("../review_delete")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
