04f1f879c39a6975f50216d3afc27b35c3aeb8c1
CatAisa <suzuki03331@icloud.com>
2021-02-09 21:50:08 +0900

    コメントアウトの削除

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,19 +7,12 @@ class CommentsController < ApplicationController
   def create
     @procedure = @manual.procedures.find(params[:procedure_id])
     @comment = @procedure.comments.new(comment_params)
-    # @comment[:manual_id] = @manual.id
     if @comment.save
       render json: { comment: @comment, user: current_user }
     else
       @procedures = @manual.procedures.includes(:user)
       render 'manuals/show'
     end
-    # if @comment.save
-    #   redirect_to manual_path(@manual)
-    # else
-    #   @procedures = @manual.procedures.includes(:user)
-    #   render 'manuals/show'
-    # end
   end
 
   def destroy
