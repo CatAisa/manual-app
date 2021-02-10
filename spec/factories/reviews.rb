0d6d0a188afd7281e457105c3593698017ac8ae6
CatAisa <suzuki03331@icloud.com>
2021-02-10 21:16:09 +0900

    Reviewモデルのテストコードを実施

--- spec/factories/reviews.rb
+++ spec/factories/reviews.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :review do
-    
+    content { Faker::Lorem.sentences }
+
+    association :user
+    association :manual
   end
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Review, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @review = FactoryBot.build(:review)
+  end
+
+  describe 'レビュー新規作成' do
+    context 'レビュー情報を保存できるとき' do
+      it 'content, user_id, manual_idが存在すれば保存できる' do
+        expect(@review).to be_valid
+      end
+    end
+
+    context 'レビュー情報を保存できないとき' do
+      it 'contentが空だと保存できない' do
+        @review.content = nil
+        @review.valid?
+        expect(@review.errors.full_messages).to include('Content translation missing: ja.activerecord.errors.models.review.attributes.content.blank')
+      end
+      it 'Userが紐付いていないと保存できない' do
+        @review.user = nil
+        @review.valid?
+        expect(@review.errors.full_messages).to include('User translation missing: ja.activerecord.errors.models.review.attributes.user.required')
+      end
+      it 'Manualが紐付いていないと保存できない' do
+        @review.manual = nil
+        @review.valid?
+        expect(@review.errors.full_messages).to include('Manual translation missing: ja.activerecord.errors.models.review.attributes.manual.required')
+      end
+    end
+  end
 end
