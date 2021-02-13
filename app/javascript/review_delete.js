179b948cc0222b22a78b45cec7a1c44415feeafe
CatAisa <suzuki03331@icloud.com>
2021-02-13 09:23:59 +0900

    記述を微修正

--- app/javascript/review_delete.js
+++ app/javascript/review_delete.js
@@ -11,7 +11,7 @@ function reviewDelete() {
 };
 
 if (document.URL.match(/manuals/)) {
-  if (!document.URL.match(/new/) && !document.URL.match(/edit/)){
+  if (!document.URL.match(/new/) && !document.URL.match(/edit/)) {
     window.addEventListener("load", reviewDelete);
     setInterval(reviewDelete, 1000);
   };
--- app/javascript/review_post.js
+++ app/javascript/review_post.js
@@ -34,7 +34,7 @@ function reviewPost() {
 };
 
 if (document.URL.match(/manuals/)) {
-  if (!document.URL.match(/new/) && !document.URL.match(/edit/)){
+  if (!document.URL.match(/new/) && !document.URL.match(/edit/)) {
     window.addEventListener("load", reviewPost);
   };
 };
