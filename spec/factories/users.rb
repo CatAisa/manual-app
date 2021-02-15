8673bdf76b6305819d5ef93fd51fb0d3dec8c611
CatAisa <suzuki03331@icloud.com>
2021-02-15 13:43:39 +0900

    Fakerによるpassword作成を英数混合に変更

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname { 'abcde12345' }
     email { Faker::Internet.free_email }
-    password { Faker::Internet.password(min_length: 8) }
+    password { Faker::Internet.password(min_length: 8, mix_case: true) }
     password_confirmation { password }
   end
 end
