dac4157e2139903cb00553636ecc1b48f853b64d
CatAisa <suzuki03331@icloud.com>
2021-02-15 19:27:43 +0900

    FactoryBotの修正

--- spec/factories/manuals.rb
+++ spec/factories/manuals.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :manual do
     title { Faker::Lorem.sentences(number: 1) }
-    category_id { Faker::Number.between(from: 1, to: 10) }
+    category_id { Faker::Number.between(from: 1, to: 4) }
     description { Faker::Lorem.sentences }
 
     association :user
