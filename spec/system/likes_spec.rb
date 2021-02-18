3e02593c22a1e1828e5a818c4f2637d8d10c1608
CatAisa <suzuki03331@icloud.com>
2021-02-18 23:12:32 +0900

    お気に入り解除の結合テストコードを実施

--- spec/system/likes_spec.rb
+++ spec/system/likes_spec.rb
@@ -42,3 +42,46 @@ RSpec.describe 'お気に入り登録', type: :system do
     end
   end
 end
+
+RSpec.describe 'お気に入り解除', type: :system do
+  before do
+    @manual1 = FactoryBot.create(:manual)
+    @release1 = FactoryBot.create(:release, user_id: @manual1.user.id, manual_id: @manual1.id)
+    @manual2 = FactoryBot.create(:manual)
+    @release2 = FactoryBot.create(:release, user_id: @manual2.user.id, manual_id: @manual2.id)
+    @like1 = FactoryBot.create(:like, user_id: @manual2.user.id, manual_id: @manual1.id)
+    @like2 = FactoryBot.create(:like, user_id: @manual1.user.id, manual_id: @manual2.id)
+  end
+
+  context 'お気に入り解除できるとき' do
+    it 'お気に入り登録したユーザーはお気に入り解除できる' do
+      # manual1のユーザーでログインする
+      sign_in(@manual1.user)
+      # トップページに公開されているマニュアルの情報が表示されている
+      check_manual(@manual1.title, @manual1.category.name, @manual1.description)
+      check_manual(@manual2.title, @manual2.category.name, @manual2.description)
+      # お気に入りページに遷移する
+      move_likepage(@manual1)
+      # お気に入りページにmanual2の情報が表示されている
+      check_manual(@manual2.title, @manual2.category.name, @manual2.description)
+      # manual2にお気に入り解除ボタンが存在する
+      expect(page).to have_content('★')
+      # お気に入り解除ボタンをクリックすると、Likeモデルのカウントが1減少する
+      expect {
+        click_on('★')
+      }.to change { Like.count }.by(-1)
+      # トップページに遷移する
+      expect(current_path).to eq(root_path)
+      # お気に入りページに遷移する
+      move_likepage(@manual1)
+      # お気に入りページに先ほど解除したマニュアルの情報が存在しない
+      check_no_manual(@manual2.title, @manual2.category.name, @manual2.description)
+    end
+  end
+
+  context 'お気に入り解除できないとき' do
+    it 'お気に入り登録したユーザー以外はお気に入り解除できない' do
+      
+    end
+  end
+end
