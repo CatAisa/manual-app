99d44a61222244b1ec34424e467fbadee5134da5
CatAisa <suzuki03331@icloud.com>
2021-02-21 12:10:11 +0900

    レビュー・コメント投稿統合の雛形作成

--- app/javascript/comment_post.js
+++ app/javascript/comment_post.js
@@ -1,6 +1,48 @@
 function commentPostTitle() {
-  commentPost();
+  const submitCommentAll = document.querySelectorAll(".comment-submit-btn");
+  submitCommentAll.forEach((submitComment) => {
+    commentPostTest(submitComment);
+  });
+  // commentPost();
   reviewPost();
+};
+
+function commentPostTest(submit) {
+  submit.addEventListener("click", (e) => {
+    e.preventDefault();
+    const manualId = submit.getAttribute("manual_id");
+    const procedureId = submit.getAttribute("procedure_id");
+    const formId = `comment-form${procedureId}`;
+    const textId = `comment-text${procedureId}`;
+    const formData = new FormData(document.getElementById(formId));
+    const XHR = new XMLHttpRequest();
+    XHR.open("POST", `/manuals/${manualId}/procedures/${procedureId}/comments`, true);
+    XHR.responseType = "json";
+    XHR.send(formData);
+    XHR.onload = () => {
+      if (XHR.status != 200) {
+        alert(`Error ${XHR.status}: ${XHR.statusText}`);
+        return null;
+      };
+      const comment = XHR.response.comment;
+      const user = XHR.response.user;
+      const listId = `list${procedureId}`;
+      const list = document.getElementById(listId);
+      const commentId = `${comment.id}`;
+      const formText = document.getElementById(textId);
+      const HTML = `
+        <div class="comment" style="" comment_id=${comment.id}>
+          <div class="comment-header">
+            ${user.nickname}
+            <span class="comment-time">${comment.created_at}</span>
+            <a rel ="nofollow" data-method="delete" href="/manuals/${manualId}/procedures/${procedureId}/comments/${commentId}" id="comment-delete${comment.id}">削除</a>
+          </div>
+          <div class="comment-text">${comment.content}</div>
+        </div>`;
+      list.insertAdjacentHTML("beforebegin", HTML);
+      formText.value = "";
+    };
+  });
 };
 
 function commentPost() {
