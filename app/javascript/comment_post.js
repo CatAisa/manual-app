2f65ecbb21498aac53f084103ad959ee87371d79
CatAisa <suzuki03331@icloud.com>
2021-02-21 14:45:31 +0900

    レビュー・コメント投稿統合#4

--- app/javascript/comment_post.js
+++ app/javascript/comment_post.js
@@ -5,7 +5,7 @@ function commentPostTitle() {
     const procedureId = submitComment.getAttribute("procedure_id");
     const path = `/manuals/${manualId}/procedures/${procedureId}/comments`;
     const type = 'comment';
-    commentPostTest(submitComment, procedureId, path, type);
+    commentPost(submitComment, procedureId, path, type);
   });
 
   const submitReview = document.querySelector(".review-submit-btn");
@@ -13,10 +13,10 @@ function commentPostTitle() {
   const procedureId = '';
   const path = `/manuals/${manualId}/reviews`;
   const type = 'review';
-  commentPostTest(submitReview, procedureId, path, type);
+  commentPost(submitReview, procedureId, path, type);
 };
 
-function commentPostTest(submit, procedureId, path, type) {
+function commentPost(submit, procedureId, path, type) {
   submit.addEventListener("click", (e) => {
     e.preventDefault();
     const formId = `${type}-form${procedureId}`;
@@ -45,82 +45,6 @@ function commentPostTest(submit, procedureId, path, type) {
             <a rel ="nofollow" data-method="delete" href="${path}/${modelId}" id="${type}-delete${model.id}">削除</a>
           </div>
           <div class="${type}-text">${model.content}</div>
-        </div>`;
-      list.insertAdjacentHTML("beforebegin", HTML);
-      formText.value = "";
-    };
-  });
-};
-
-function commentPost() {
-  const submits = document.querySelectorAll(".comment-submit-btn");
-  submits.forEach(function(submit) {
-    submit.addEventListener("click", (e) => {
-      e.preventDefault();
-      const manualId = submit.getAttribute("manual_id");
-      const procedureId = submit.getAttribute("procedure_id");
-      const formId = `comment-form${procedureId}`;
-      const textId = `comment-text${procedureId}`;
-      const formData = new FormData(document.getElementById(formId));
-      const XHR = new XMLHttpRequest();
-      XHR.open("POST", `/manuals/${manualId}/procedures/${procedureId}/comments`, true);
-      XHR.responseType = "json";
-      XHR.send(formData);
-      XHR.onload = () => {
-        if (XHR.status != 200) {
-          alert(`Error ${XHR.status}: ${XHR.statusText}`);
-          return null;
-        };
-        const comment = XHR.response.comment;
-        const user = XHR.response.user;
-        const listId = `list${procedureId}`;
-        const list = document.getElementById(listId);
-        const commentId = `${comment.id}`;
-        const formText = document.getElementById(textId);
-        const HTML = `
-          <div class="comment" style="" comment_id=${comment.id}>
-            <div class="comment-header">
-              ${user.nickname}
-              <span class="comment-time">${comment.created_at}</span>
-              <a rel ="nofollow" data-method="delete" href="/manuals/${manualId}/procedures/${procedureId}/comments/${commentId}" id="comment-delete${comment.id}">削除</a>
-            </div>
-            <div class="comment-text">${comment.content}</div>
-          </div>`;
-        list.insertAdjacentHTML("beforebegin", HTML);
-        formText.value = "";
-      };
-    });
-  });
-};
-
-function reviewPost() {
-  const submitReview = document.querySelector(".review-submit-btn");
-  submitReview.addEventListener("click", (e) => {
-    e.preventDefault();
-    const manualId = submitReview.getAttribute("manual_id");
-    const formData = new FormData(document.getElementById("review-form"));
-    const XHR = new XMLHttpRequest();
-    XHR.open("POST", `/manuals/${manualId}/reviews`, true);
-    XHR.responseType = "json";
-    XHR.send(formData);
-    XHR.onload = () => {
-      if (XHR.status != 200) {
-        alert(`Error ${XHR.status}: ${XHR.statusText}`);
-        return null;
-      };
-      const review = XHR.response.review;
-      const user = XHR.response.user;
-      const list = document.getElementById("review-list");
-      const reviewId = `${review.id}`;
-      const formText = document.getElementById("review-text");
-      const HTML = `
-        <div class="review" style="" review_id=${review.id}>
-          <div class="review-header">
-            ${user.nickname}
-            <span class="review-time">${review.created_at}</span>
-            <a rel ="nofollow" data-method="delete" href="/manuals/${manualId}/reviews/${reviewId}" id="review-delete${review.id}">削除</a>
-          </div>
-          <div class="review-text">${review.content}</div>
         </div>`;
       list.insertAdjacentHTML("beforebegin", HTML);
       formText.value = "";
